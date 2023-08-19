9bd2fe2f6e48dd812999a206645affd526927347
Koby <kobysac@gmail.com>
2023-08-18 23:41:44 -0700

    added collectInfo() and validateInfo()

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,37 @@
 // Assignment code here
+var criteria = {};
 
+function generatePassword() {
+  collectInfo();
+  if (!validateInfo()) {
+    if(confirm("Retry?")) {
+      generatePassword();
+    } else {
+      return "";
+    }
+  }
+  return "Test";
+}
+
+function collectInfo() {
+  criteria.minLength = prompt("Minimum character length:");
+  // console.log(typeof(criteria.minLength))
+  criteria.maxLength = prompt("Maximum password length:")
+  criteria.useLowerCase = confirm("Do you want to include lower case characters?")
+  criteria.useUpperCase = confirm("Do you want to include upper case characters?")
+  criteria.useNumeric = confirm("Do you want to include numeric values?")
+  criteria.useSpecial = confirm("Do you want to include special characters?")
+  
+}
+
+function validateInfo() {
+  goodPassword = true;
+  if(!(typeof(criteria.minLength) === 'number')  || (criteria.minLength <= 0)) {
+    alert("The minimum password length must be greater than 0.")
+    goodPassword = false;
+  }
+  if(criteria.maxLength)
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
