89739776f8bcb54fb584573df530a82c67782552
Koby <kobysac@gmail.com>
2023-08-18 23:57:19 -0700

    finished validInfo()

--- Develop/script.js
+++ Develop/script.js
@@ -13,7 +13,7 @@ function generatePassword() {
   return "Test";
 }
 
-function collectInfo() {
+function collectInfo() { //gather info for the criteria of a password
   criteria.minLength = prompt("Minimum character length:");
   // console.log(typeof(criteria.minLength))
   criteria.maxLength = prompt("Maximum password length:")
@@ -21,16 +21,26 @@ function collectInfo() {
   criteria.useUpperCase = confirm("Do you want to include upper case characters?")
   criteria.useNumeric = confirm("Do you want to include numeric values?")
   criteria.useSpecial = confirm("Do you want to include special characters?")
-  
 }
 
-function validateInfo() {
-  goodPassword = true;
-  if(!(typeof(criteria.minLength) === 'number')  || (criteria.minLength <= 0)) {
+function validateInfo() { //validate that the info in the criteria are legitimate values
+  var goodPassword = true;
+  if(!(typeof(criteria.minLength) === 'number')  || (criteria.minLength <= 0)) { //check if minLength is good
     alert("The minimum password length must be greater than 0.")
     goodPassword = false;
   }
-  if(criteria.maxLength)
+  if(!(typeof(criteria.maxLength) === 'number')  || (criteria.maxLength < criteria.minLength)) {//check if maxLength is good
+    alert("The maximum password length must be greater than or equal to the minimum length.")
+    goodPassword = false;
+  }
+  if(criteria.useLowerCase == false &&
+     criteria.useUpperCase == false &&
+     criteria.useNumeric == false &&
+     criteria.useSpecial == false) { //make sure not all the character types are false
+    alert("At least one character type must be used.")
+    goodPassword = false;
+  }
+  return goodPassword;
 }
 
 // Get references to the #generate element
