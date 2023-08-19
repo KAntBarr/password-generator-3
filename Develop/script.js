de66f8eeabf0ef8d01e2e7f14eea3fa4f21cc37f
Koby <kobysac@gmail.com>
2023-08-19 00:46:30 -0700

    added and finished createPassword()

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,24 @@
 // Assignment code here
-var criteria = {};
+var criteria = {};//global password criteria object
+var lower_case_set = "abcdefghijklmnopqrstuvwxyz";
+var upper_case_set = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numeric_set = "0123456789";
+var special_set = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
 function generatePassword() {
-  collectInfo();
-  if (!validateInfo()) {
+  collectInfo();//get info
+  if (!validateInfo()) {//validate info
     if(confirm("Retry?")) {
-      generatePassword();
+      generatePassword();//retry to get info
     } else {
-      return "";
+      return "";//else return nothing
     }
   }
-  return "Test";
+  return createPassword();//return the generated secure password
 }
 
 function collectInfo() { //gather info for the criteria of a password
   criteria.minLength = prompt("Minimum character length:");
-  // console.log(typeof(criteria.minLength))
   criteria.maxLength = prompt("Maximum password length:")
   criteria.useLowerCase = confirm("Do you want to include lower case characters?")
   criteria.useUpperCase = confirm("Do you want to include upper case characters?")
@@ -25,14 +28,16 @@ function collectInfo() { //gather info for the criteria of a password
 
 function validateInfo() { //validate that the info in the criteria are legitimate values
   var goodPassword = true;
-  if(!(typeof(criteria.minLength) === 'number')  || (criteria.minLength <= 0)) { //check if minLength is good
+  if( isNaN(criteria.minLength) || (criteria.minLength <= 0)) { //check if minLength is good
     alert("The minimum password length must be greater than 0.")
     goodPassword = false;
   }
-  if(!(typeof(criteria.maxLength) === 'number')  || (criteria.maxLength < criteria.minLength)) {//check if maxLength is good
+  criteria.minLength = Number(criteria.minLength);
+  if( isNaN(criteria.maxLength) || (criteria.maxLength < criteria.minLength)) {//check if maxLength is good
     alert("The maximum password length must be greater than or equal to the minimum length.")
     goodPassword = false;
   }
+  criteria.maxLength = Number(criteria.maxLength);
   if(criteria.useLowerCase == false &&
      criteria.useUpperCase == false &&
      criteria.useNumeric == false &&
@@ -41,6 +46,24 @@ function validateInfo() { //validate that the info in the criteria are legitimat
     goodPassword = false;
   }
   return goodPassword;
+}
+
+function createPassword() {//create the password with the given criteria
+  var password_set = ""; //add corresponding character sets to password set
+  if(criteria.useLowerCase) password_set += lower_case_set;
+  if(criteria.useUpperCase) password_set += upper_case_set;
+  if(criteria.useNumeric) password_set += numeric_set;
+  if(criteria.useSpecial) password_set += special_set;
+  criteria.passwordLength = randomIntFromInterval(criteria.minLength, criteria.maxLength);//get random password length
+  var securePassword = "";
+  for(var i = 0; i<criteria.passwordLength; i++){//loop through and add a random character to the password per iteration
+    securePassword += password_set[randomIntFromInterval(0, password_set.length)];
+  }
+  return securePassword;
+}
+
+function randomIntFromInterval(min, max) { //gets a random integer within an interval
+  return Math.floor(Math.random() * (max - min + 1) + min)// min and max included 
 }
 
 // Get references to the #generate element
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Password Generator Starter Code
+
+
+
+
+## Resources
+https://stackoverflow.com/questions/1497481/javascript-password-generator
+https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
